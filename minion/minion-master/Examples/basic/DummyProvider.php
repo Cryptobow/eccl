c330d7547b8373bb5531d66f9410f29da47d60d3
Cryptobow <30305180+Cryptobow@users.noreply.github.com>
2017-07-19 21:36:22 -0500

    Add files via upload

---
+++ minion/minion-master/Examples/basic/DummyProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Vinelab\Minion;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+// Provider's namespace is Vinelab\Minion\Provider
+class DummyProvider extends Provider
+{
+    protected $prefix = 'com.example.';
+
+    public function boot()
+    {
+        $this->register('getphpversion', 'getPhpVersion');
+        $this->subscribe('pripra', 'papa');
+    }
+
+    public function getPhpVersion()
+    {
+        $this->publish('asked', null, ['send' => 'thing']);
+
+        return phpversion();
+    }
+
+    public function papa()
+    {
+        var_dump('PAPA CALLED!');
+    }
+}
---
+++ minion/minion-master/src/Vinelab/Minion/InvalidProviderException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vinelab\Minion;
+
+use Exception;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+class InvalidProviderException extends Exception
+{
+}
